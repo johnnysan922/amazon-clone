ab6b06fb05c00449d6444b07d7b56cefbe01d5c7
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-06 10:50:35 -0500

    Set up firebase database

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBlxhqGg3HNlLm2NuXHb8tFDZhKcMgUR4s",
+    authDomain: "clone-1f7fc.firebaseapp.com",
+    projectId: "clone-1f7fc",
+    storageBucket: "clone-1f7fc.appspot.com",
+    messagingSenderId: "295827600036",
+    appId: "1:295827600036:web:50a6351fb7017bdf3bec31",
+    measurementId: "G-LZX8M20NCJ"
+  };
