37b78488af902d35cd73708849d94ca4a5db8afa
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-06 11:04:47 -0500

    Set 'header' and 'home' components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //BEM (change "App" to "app")
+    <div className="app">
+      <h1> Hello Johnny, lets build this amazon site!</h1>
+      {/* HEADER */}
+      {/* {HOME} */}
     </div>
   );
 }
