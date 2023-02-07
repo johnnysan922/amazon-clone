75c12cae33a44a22249f7815af6f9aae6a6a83c5
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-07 13:07:03 -0500

    Cart now displays number of items in basket

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,11 @@ import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import { Link } from 'react-router-dom'
+import { useStateValue } from './StateProvider';
 
 function Header() {
+    const [{basket}, dispatch] = useStateValue();
+
   return (
     // head will contain (left to right): image, search bar, etc (children)
     <div className='header'>
@@ -42,7 +45,9 @@ function Header() {
             <Link to='/checkout'>
                 <div className='header_optionBasket'>
                     <ShoppingBasketIcon />
-                    <span className='header_optionLine2 header_basketCount'>0</span>
+                    <span className='header_optionLine2 header_basketCount'>
+                        {basket?.length}
+                    </span>
                 </div>
             </Link>
         </div>
