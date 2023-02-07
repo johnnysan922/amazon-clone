fb22be9118c857190f4ea452e0063e5f68f11e60
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-07 15:35:24 -0500

    Helps send data to data layer

--- src/reducer.js
+++ src/reducer.js
@@ -11,8 +11,27 @@ const reducer = (state, action) => {
         case 'ADD_TO_BASKET':
             return{
                 ...state,
-                basket: [...state.basket, action.item]
+                basket: [...state.basket, action.item],
             };
+
+        case 'REMOVE_FROM_BASKET':
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            let newBasket = [...state.basket];
+
+            if(index >= 0){
+                newBasket.splice(index, 1);     //cuts basket by 1 where item was
+            }else{
+                console.warn(
+                    `Can't remove product (id: ${action.id}) as it is not in basket.`
+                )
+            }
+            return{
+                ...state,
+                basket: newBasket
+            }
+
         default:
             return state;
     }
