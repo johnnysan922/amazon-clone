40e9895e57a1add64458888be28c16f0583301bb
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-07 13:15:36 -0500

    Subtotal displays number of items in basket

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format'
+import { useStateValue } from './StateProvider';
 
 function Subtotal() {
+    const [{basket}, dispatch] = useStateValue();
+
   return (
     <div className='subtotal'>
         <CurrencyFormat
             renderText={(value) =>(
                 <>
                     <p>
-                        Subtotal (0 items): <strong>0</strong>
+                        Subtotal ({basket?.length} items): 
+                        <strong>
+                            0
+                        </strong>
                     </p>
                     <small className='subtotal_gift'>
                         <input type='checkbox' /> This order contains a gift 
