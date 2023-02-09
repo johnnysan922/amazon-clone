d04398cccce8970b56c638d4a9b4d26861c46e38
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-09 17:12:25 -0500

    Added styling and other stuff

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,6 +6,7 @@ import { useStateValue } from './StateProvider'
 
 function Checkout() {
     const [{ basket, user }, dispatch] = useStateValue();
+    
   return (
     <div className='checkout'>
         <div className='checkout_left'>
--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './CheckoutProduct.css'
 import { useStateValue } from './StateProvider'
 
-function CheckoutProduct({id, image, title, price, rating}) {
+function CheckoutProduct({id, image, title, price, rating, hideButton}) {
     const [{basket}, dispatch] = useStateValue();
 
     const removeFromBasket = () => {
@@ -29,7 +29,10 @@ function CheckoutProduct({id, image, title, price, rating}) {
                 ))}
             </div>
 
-            <button onClick={removeFromBasket}>Remove from basket</button>
+            {!hideButton && (
+                <button onClick={removeFromBasket}>Remove from basket</button>
+            )}
+            
         </div>
     </div>
   )
