ec8087755adc4c065bd352228ea6c5539883f639
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-10 13:31:22 -0500

    Changed baseurl from local url to firebase api url

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
     //API (cloud function) URL
-    baseURL: 'http://127.0.0.1:5001/clone-1f7fc/us-central1/api'
+    baseURL: 'https://us-central1-clone-1f7fc.cloudfunctions.net/api'
 })
 // TESTING URL(local)
 // http://127.0.0.1:5001/clone-1f7fc/us-central1/ap
